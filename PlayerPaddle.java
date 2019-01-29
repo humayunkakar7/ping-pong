d625a12a3bedcb04ace493bb32eddd37167ab649
Humayun Khan <humayun.khan@mail.mcgill.ca>
2019-01-29 14:09:43 -0500

    player paddle done

--- PlayerPaddle.java
+++ PlayerPaddle.java
@@ -26,4 +26,19 @@ public class PlayerPaddle {
 		boundingBox.setBounds(x, y, width, height);
 	}
 
+	public void tick(Game game){
+		boundingBox.setBounds(x, y, width, height);
+		
+		if(goingUp && y>20){
+			y-= speed; //y-coordinate decreases 
+		} else if (goingDown && y + height <= game.HEIGHT){
+			y+= speed; //y-coordinate increases
+		}
+	}
+
+	public void render(Graphics g){
+		//rendering the white block 
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, width, height);
+	}
 }
